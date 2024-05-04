2392b3ed199464923c6b7695ec192f45d1c31815
MartinaZabala <martina.zabala@alumnos.frm.utn.edu.ar>
2024-05-04 16:03:33 -0300

    Guardar cambios locales en store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,15 @@
-// Importación necesaria
 import { configureStore } from "@reduxjs/toolkit";
-import TablaReducer from "./slices/TablaReducer"; // Importamos el reducer del slice TablaReducer
+import TablaReducer from "./slices/TablaReducer";
 import articuloManufacturadoSlice from "./slices/ArticuloManufacturado";
+import usuariosSlice from "./slices/Usuario"; // Importamos el nuevo slice de usuarios
 
-// Configuración de la tienda de Redux
 export const store = configureStore({
   reducer: {
-    tablaReducer: TablaReducer, // Agregamos el reducer del slice TablaReducer al estado global con la clave tablaReducer
-    articuloManufacturado: articuloManufacturadoSlice
+    tablaReducer: TablaReducer,
+    articuloManufacturado: articuloManufacturadoSlice,
+    usuarios: usuariosSlice, // Agregamos el nuevo slice de usuarios al estado global con la clave usuarios
   },
 });
 
-// Inferimos los tipos `RootState` y `AppDispatch` del almacén de la tienda misma
 export type RootState = ReturnType<typeof store.getState>;
-// Tipo inferido: { modalReducer: ModalState, tablaReducer: TablaState }
 export type AppDispatch = typeof store.dispatch;
