34f322003277a2c2f69c5f89f5053dff445c2bd2
roAscurra <rominaascurra4887@gmail.com>
2024-05-06 08:43:50 -0300

    Rutas

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,54 +1,36 @@
-import React from 'react';
-import Sidebar from '../components/Sider/SideBar';
-import { Route, Routes } from 'react-router-dom';
-import { ListaProductos } from '../components/Productos/ListaProductos';
-import { CContainer, CRow, CCol } from "@coreui/react";
+import Sidebar from '../components/Sider/SideBar'
+import { Route, Routes } from 'react-router-dom'
+import { ListaProductos } from '../components/Productos/ListaProductos'
+import { CContainer, CRow, CCol } from "@coreui/react"; // Importa los componentes de diseño de CoreUI
 import Inicio from '../components/Inicio/Inicio';
 import Usuario from '../components/Usuario/Usuario';
 import { ListaCupones } from '../components/Marketing/Cupones';
-import { Promocion } from '../components/Marketing/Promociones';
 import Estadisticas from '../components/Estadisticas/Estadisticas';
-import BaseNavBar from '../components/common/BaseNavBar';
-import Promociones from '../types/Promocion';
+import Promocion from '../components/Marketing/Promociones';
 
-
-const AppRouter: React.FC = () => {
-  const navLinks = [
-    { title: 'Inicio', to: '/' },
-    { title: 'Usuario', to: '/usuario' },
-    { title: 'Estadísticas', to: '/estadisticas' },
-    { title: 'Productos', to: '/productos/lista' },
-    { title: 'Cupones', to: '/cupones/lista' },
-    { title: 'Promocion', to: '/promociones/lista' },
-
-  ];
-
+export const AppRouter = () => {
   return (
-    <React.Fragment>
-      <BaseNavBar /> {/* Agregar BaseNavBar aquí */}
-      <CContainer fluid>
-        <CRow>
-          {/* Sidebar */}
-          <CCol xs="auto" className="sidebar">
-            <Sidebar />
-          </CCol>
+    <CContainer fluid>
+      <CRow>
+        {/* Sidebar */}
+        <CCol xs="auto" className="sidebar">
+          <Sidebar />
+        </CCol>
 
-          {/* Contenido principal */}
-          <CCol>
-            <Routes>
-              <Route path="/" element={<Inicio />} />
-              <Route path="/usuario" element={<Usuario />} />
-              <Route path="/estadisticas" element={<Estadisticas />} />
-              <Route path="/productos/lista" element={<ListaProductos />} />
-              <Route path="/cupones/lista" element={<ListaCupones />} />
-              <Route path="/promociones/lista" element={<Promocion />} />
+        {/* Contenido principal */}
+        <CCol>
+          <Routes>
+            <Route path ="/" element={<Inicio/>}/>
+            <Route path ="/usuario" element={<Usuario/>}/>
+            <Route path ="/estadisticas" element={<Estadisticas />}/>
+            <Route path="/productos/lista" element={<ListaProductos />} />
+            <Route path="/cupones/lista" element={<ListaCupones />} />
+            <Route path="/promociones/lista" element={<Promocion />} />
 
-              {/* Agrega más rutas aquí */}
-              <Route path="/categorias" element={<Categoria />} />
+            {/* Agrega más rutas aquí */}
           </Routes>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </React.Fragment>
-  );
-};
+        </CCol>
+      </CRow>
+    </CContainer>
+  )
+}
