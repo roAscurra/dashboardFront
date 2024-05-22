5b0cd4672412039ff194f0699baabe11bd7929d5
FlorGubiotti <florgubiotti@gmail.com>
2024-05-22 17:48:41 -0300

    appRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
-import { CContainer, CRow, CCol } from '@coreui/react';
-import Sidebar from './components/ui/Sider/SideBar';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 
 const AppContent = () => {
-  const location = useLocation();
-  const hideSidebarPaths = ["/"]; // Ocultar sidebar en la ruta raíz
-
-  const shouldShowSidebar = !hideSidebarPaths.some(path => location.pathname === path);
-
   return (
-    <CContainer fluid>
-      <CRow>
-        {shouldShowSidebar && (
-          <CCol xs="auto" className="sidebar">
-            <Sidebar />
-          </CCol>
-        )}
-        <CCol>
-          <AppRouter />
-        </CCol>
-      </CRow>
-    </CContainer>
+    <AppRouter />
   );
 }
 
