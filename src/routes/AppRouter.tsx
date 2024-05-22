8d9583647fb25d69899b85b420ebc9fe71bbc692
roAscurra <rominaascurra4887@gmail.com>
2024-05-22 18:04:56 -0300

    Rutas

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,10 +3,10 @@ import { BaseNavBar } from '../components/ui/common/BaseNavBar';
 import {ListaArticulosInsumo} from "../components/screens/ArticuloInsumo/ArticuloInsumo.tsx";
 import Inicio from "../components/screens/Inicio/Inicio.tsx";
 import Usuario from "../components/screens/Usuario/Usuario.tsx";
-import Estadisticas from "../components/screens/Estadisticas/Estadisticas.tsx";
+// import Estadisticas from "../components/screens/Estadisticas/Estadisticas.tsx";
 import {ListaProductos} from "../components/screens/Productos/ListaProductos.tsx";
 import Categoria from "../components/screens/Categoria/Categoria.tsx";
-import {ListaCupones} from "../components/screens/Marketing/Cupones/Cupones.tsx";
+// import {ListaCupones} from "../components/screens/Marketing/Cupones/Cupones.tsx";
 import {ListaPromocion} from "../components/screens/Marketing/Promociones/Promocion.tsx";
 import {ListaSucursal} from "../components/screens/Sucursal/Sucursal.tsx";
 import {ListaEmpresa} from "../components/screens/Empresa/Empresa.tsx";
@@ -17,13 +17,13 @@ const AppRouter = () => {
     <Routes>
       <Route path="/" element={<><BaseNavBar title="Empresas" /> <ListaEmpresa /></>} />
       <Route path='/ArticuloInsumo/Lista' element={<ListaArticulosInsumo />} />
-      <Route path="/inicio" element={<Inicio />} />
-      <Route path="/usuario" element={<Usuario />} />
-      <Route path="/estadisticas" element={<Estadisticas />} />
-      <Route path="/productos/lista" element={<ListaProductos />} />
-      <Route path="/categorias" element={<Categoria />} />
-      <Route path="/cupones/lista" element={<ListaCupones />} />
-      <Route path="/promociones/lista" element={<ListaPromocion />} />
+      <Route path="/inicio/:sucursalId" element={<Inicio />} />
+      <Route path="/usuario/:sucursalId" element={<Usuario />} />
+      {/* <Route path="/estadisticas" element={<Estadisticas />} /> */}
+      <Route path="/productos/lista/:sucursalId" element={<ListaProductos />} />
+      <Route path="/categorias/:sucursalId" element={<Categoria />} />
+      {/* <Route path="/cupones/lista" element={<ListaCupones />} /> */}
+      <Route path="/promociones/lista/:sucursalId" element={<ListaPromocion />} />
       <Route path="/sucursal/:empresaId" element={<><BaseNavBar title="Sucursales" /><ListaSucursal /></>} />
     </Routes>
   );
