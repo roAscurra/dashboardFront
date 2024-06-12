0e90f9839e47406bb56c9c4fa7d24787a02ee9af
roAscurra <rominaascurra4887@gmail.com>
2024-06-12 10:46:22 -0300

    Categorias de sucursal

--- src/services/CategoriaService.ts
+++ src/services/CategoriaService.ts
@@ -1,4 +1,26 @@
+import Categoria from "../types/Categoria";
 import ICategoria from "../types/Categoria";
 import  BackendClient  from "./BackendClient";
 
-export default class CategoriaService extends BackendClient<ICategoria> {}
+export default class CategoriaService extends BackendClient<ICategoria> {
+    public async categoriaSucursal(url: string, idSucursal: number, token: string): Promise<Categoria[]> {
+        try {
+          const path = `${url}categoria/sucursal/${idSucursal}`;
+          const response = await fetch(path, { method: "GET" ,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },});
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+      
+          const data = await response.json();
+          return data as Categoria[];
+        } catch (error) {
+          console.error("Error al obtener los pedidos del cliente:", error);
+          throw error;
+        }
+      }
+}
