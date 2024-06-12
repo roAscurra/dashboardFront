be05022451af0220f8ead682a7ec28f6ef001bfe
FlorGubiotti <florgubiotti@gmail.com>
2024-06-12 16:57:01 -0300

    filtrar pedidos y cambiar estado

--- src/services/PedidoService.ts
+++ src/services/PedidoService.ts
@@ -4,16 +4,29 @@ import BackendClient from "./BackendClient";
 
 export default class PedidoService extends BackendClient<Pedido> {
 
-    async cambiarEstado(url: string, pedidoId: string, nuevoEstado: Estado): Promise<Pedido> {
-        const path = `${url}/${pedidoId}/estado?nuevoEstado=${nuevoEstado}`;
-        console.log(path)
-        const options: RequestInit = {
-          method: "PUT",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        };
-        return this.request(path, options);
-      }
+  async cambiarEstado(url: string, pedidoId: string, nuevoEstado: Estado, token: string): Promise<Pedido> {
+    const path = `${url}/${pedidoId}/estado?nuevoEstado=${nuevoEstado}`;
+    console.log(path)
+    const options: RequestInit = {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    };
+    return this.request(path, options);
+  }
+
+  async getPedidosFiltrados(url: string, rol: string, token: string): Promise<Pedido[]> {
+    const path = `${url}/filtrado?rol=${rol}`;
+    const options: RequestInit = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    };
+    console.log(`GET Request to: ${path}`);
+    return this.requestAll(path, options);
+  }
+
+  
 }
