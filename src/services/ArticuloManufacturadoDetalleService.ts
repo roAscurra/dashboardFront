ee64afd82e8be114a77215bee4adcb814cf32a19
roAscurra <rominaascurra4887@gmail.com>
2024-06-12 10:23:26 -0300

    Alta y modificacion de articuloManufacturado

--- src/services/ArticuloManufacturadoDetalleService.ts
+++
@@ -1,4 +0,0 @@
-import  BackendClient  from "./BackendClient";
-import ArticuloManufacturadoDetalle from "../types/ArticuloManufacturadoDetalle.ts";
-
-export default class ArticuloManufacturadoDetalleService extends BackendClient<ArticuloManufacturadoDetalle> {}
