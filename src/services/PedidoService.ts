4db1bc8db94029d07fd1b9504f438d0564d5e885
roAscurra <rominaascurra4887@gmail.com>
2024-06-12 11:26:38 -0300

    Pedidos por sucursal

--- src/services/PedidoService.ts
+++ src/services/PedidoService.ts
@@ -3,7 +3,26 @@ import { Estado } from "../types/enums/Estado";
 import BackendClient from "./BackendClient";
 
 export default class PedidoService extends BackendClient<Pedido> {
-
+    public async pedidosSucursal(url: string, idSucursal: number, token: string): Promise<Pedido[]> {
+        try {
+          const path = `${url}pedido/sucursal/${idSucursal}`;
+          const response = await fetch(path, { method: "GET" ,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },});      
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+      
+          const data = await response.json();
+          return data as Pedido[];
+        } catch (error) {
+          console.error("Error al obtener los pedidos del cliente:", error);
+          throw error;
+        }
+      }
     async cambiarEstado(url: string, pedidoId: string, nuevoEstado: Estado): Promise<Pedido> {
         const path = `${url}/${pedidoId}/estado?nuevoEstado=${nuevoEstado}`;
         console.log(path)
