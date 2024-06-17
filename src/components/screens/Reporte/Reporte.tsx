9c07a98e4f75db8ad9c6f043c9b90eaf1d16cc1d
roAscurra <rominaascurra4887@gmail.com>
2024-06-17 19:35:48 -0300

    Elimino scroll eje x en reportes

--- src/components/screens/Reporte/Reporte.tsx
+++ src/components/screens/Reporte/Reporte.tsx
@@ -4,7 +4,6 @@ import {useState} from "react";
 import {useParams} from "react-router-dom";
 import {Graphs} from "../../ui/Graphs/Graphs.tsx";
 import Sidebar from "../../ui/Sider/SideBar.tsx";
-import { CCol } from "@coreui/react";
 
 export const Reporte = () => {
 
@@ -31,70 +30,72 @@ export const Reporte = () => {
 
 
 
-    return <>
-        <BaseNavBar title="Sucursales" />
-        <Row>
-            <CCol xs="auto" className="sidebar">
-            <Sidebar />
-            </CCol>
-            <Col>
-                <div style={{padding: "1rem"}}>
-                    <h2>Reportes basados en tiempo</h2>
-                    <p>En formato XLSX</p>
-                    <Row>
-                        <Col>
-                            <div>
-                                <Form.Label htmlFor="inputPassword5">Desde</Form.Label>
-                                <Form.Control
-                                    type="date"
-                                    id="desde"
-                                    aria-describedby="desde"
-                                    onChange={(e) => setDesde(e)}
-                                />
-                                <Form.Text id="desde" muted>
-                                    Ingrese la fecha desde para generar el reporte
-                                </Form.Text>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div>
-                                <Form.Label htmlFor="inputPassword5">Hasta</Form.Label>
-                                <Form.Control
-                                    type="date"
-                                    id="hasta"
-                                    aria-describedby="hasta"
-                                    onChange={(e) => setHasta(e)}
-                                />
-                                <Form.Text id="hasta" muted>
-                                    Ingrese la fecha hasta para generar el reporte
-                                </Form.Text>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className={"p-3"}>
-                        <Col>
-                            <a style={{width: "100%", backgroundColor: "#9c27b0"}} className="btn text-light"
-                               onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/insumos/excel')}>Descargar ranking productos</a>
-                        </Col>
-                        <Col>
-                            <a style={{width: "100%", backgroundColor: "#9c27b0"}} className="btn text-light"
-                               onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/pedidos/cliente/excel')}>Descargar pedidos por cliente</a>
-                        </Col>
-                    </Row>
-                    <Row className={"p-3"}>
-                        <Col>
-                            <a style={{width: "100%", backgroundColor: "#9c27b0"}} className="btn text-light"
-                               onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/ingresos/excel')}>Descargar ingresos</a>
-                        </Col>
-                        <Col>
-                            <a style={{width: "100%", backgroundColor: "#9c27b0"}} className="btn text-light"
-                               onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/ganancias/excel')}>Descargar ganancias</a>
-                        </Col>
-                    </Row>
-                    <hr/>
-                    <Graphs />
-                </div>
-            </Col>
-        </Row>
-    </>
+    return (
+        <>
+            <BaseNavBar title="Sucursales" />
+            <Row noGutters style={{ overflowX: "hidden", width: "100%" }}>
+                <Col xs="auto" className="sidebar">
+                    <Sidebar />
+                </Col>
+                <Col>
+                    <div style={{ padding: "1rem", overflowX: "hidden" }}>
+                        <h2>Reportes basados en tiempo</h2>
+                        <p>En formato XLSX</p>
+                        <Row>
+                            <Col>
+                                <div>
+                                    <Form.Label htmlFor="inputDesde">Desde</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                        id="desde"
+                                        aria-describedby="desde"
+                                        onChange={(e) => setDesde(e.target.value)}
+                                    />
+                                    <Form.Text id="desde" muted>
+                                        Ingrese la fecha desde para generar el reporte
+                                    </Form.Text>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div>
+                                    <Form.Label htmlFor="inputHasta">Hasta</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                        id="hasta"
+                                        aria-describedby="hasta"
+                                        onChange={(e) => setHasta(e.target.value)}
+                                    />
+                                    <Form.Text id="hasta" muted>
+                                        Ingrese la fecha hasta para generar el reporte
+                                    </Form.Text>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row className={"p-3"}>
+                            <Col>
+                                <a style={{ width: "100%", backgroundColor: "#9c27b0" }} className="btn text-light"
+                                   onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/insumos/excel')}>Descargar ranking productos</a>
+                            </Col>
+                            <Col>
+                                <a style={{ width: "100%", backgroundColor: "#9c27b0" }} className="btn text-light"
+                                   onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/pedidos/cliente/excel')}>Descargar pedidos por cliente</a>
+                            </Col>
+                        </Row>
+                        <Row className={"p-3"}>
+                            <Col>
+                                <a style={{ width: "100%", backgroundColor: "#9c27b0" }} className="btn text-light"
+                                   onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/ingresos/excel')}>Descargar ingresos</a>
+                            </Col>
+                            <Col>
+                                <a style={{ width: "100%", backgroundColor: "#9c27b0" }} className="btn text-light"
+                                   onClick={() => generarExcel(sucursalId, desde, hasta, 'ranking/ganancias/excel')}>Descargar ganancias</a>
+                            </Col>
+                        </Row>
+                        <hr/>
+                        <Graphs />
+                    </div>
+                </Col>
+            </Row>
+        </>
+    );
 }
